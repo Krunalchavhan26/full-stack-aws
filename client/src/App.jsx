f4cdf4a6f4989d53d65b4ccbeb09a55c22de1f57
krunal-ekayaa <krunal@ekayaa.co.in>
2025-11-04 23:52:55 +0530

    front end url change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/message")
+    fetch("http://34.224.57.156//api/message")
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error(error));
